9b2906fc9a85d8b30b544bcb024ff345259770b1
bumman <anhbumbk@gmail.com>
2021-08-30 15:53:07 +0700

    add README.md

--- src/ulti/postgresql.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    arrayPostgreToObject: (postgre) => {
-        return postgre.map(postgre => postgre.toObject());
-    },
-    postgreToObject: (postgre) => {
-        return postgre ? postgre.toObject() : postgre;
-    }
-}
